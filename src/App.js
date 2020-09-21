2416d1dc3f865c73a3d089abd888d0077b974975
bineetNaidu <bineet.s.naidu@gmail.com>
2020-09-21 21:10:33 +0530

    add base EDITOR component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import Editor from "./Editor";
 
 // STATICS
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let Build a Codepen Clone</h1>
-    </div>
+    <>
+      <div className="pane top-pane">
+        <Editor language="xml" label="HTML" onChange={} value={} />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder={0}
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   );
 }
 
---
+++ src/Editor.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+
+// STATICS
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml"; // HTML
+import "codemirror/mode/javascript/javascript"; // JS
+import "codemirror/mode/css/css"; // CSS
+
+function Editor({ label, language, value, onChange }) {
+  const handleChange = (editor, data, value) => onChange(value);
+  return (
+    <div className="editor-container">
+      <div className="editor-title">
+        {label}
+        <button>O/C</button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          lineNumbers: true,
+          theme: "material",
+        }}
+      />
+    </div>
+  );
+}
+
+export default Editor;
